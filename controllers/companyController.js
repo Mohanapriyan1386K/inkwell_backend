c188e900d68e99b14bba529933d1f48413eaa073
Mohanapriyan1386K <mohanapriyan.k.mfedu@gmail.com>
2026-05-30 11:56:31 +0530

    assa

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -8,24 +8,32 @@ export async function getCompany(req, res) {
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
     const search = req.query.search || "";
+    const location = req.query.location || "";
 
     const skip = (page - 1) * limit;
 
-    const filter = search
-      ? {
-          $or: [
-            { companyname: { $regex: search, $options: "i" } },
-            { location: { $regex: search, $options: "i" } },
-          ],
-        }
-      : {};
+    const filter = {};
+
+    if (search) {
+      filter.companyname = {
+        $regex: search,
+        $options: "i",
+      };
+    }
+
+    if (location) {
+      filter.location = {
+        $regex: location,
+        $options: "i",
+      };
+    }
 
     const totalCompanies = await Company.countDocuments(filter);
 
     const companies = await Company.find(filter)
+      .sort({ createdAt: -1 })
       .skip(skip)
-      .limit(limit)
-      .sort({ createdAt: -1 });
+      .limit(limit);
 
     res.status(200).json({
       success: true,
@@ -38,7 +46,7 @@ export async function getCompany(req, res) {
       },
     });
   } catch (err) {
-    console.log(err);
+    console.error(err);
 
     res.status(500).json({
       success: false,
