77ef362ad429e105a4ec2ef2342c250e2f1fe1a2
Mohanapriyan1386K <mohanapriyan.k.mfedu@gmail.com>
2026-06-01 17:25:30 +0530

    30

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -6,7 +6,7 @@ import Company from "../models/Company.js";
 export async function getCompany(req, res) {
   try {
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 30;
     const search = req.query.search || "";
     const location = req.query.location || "";
 
