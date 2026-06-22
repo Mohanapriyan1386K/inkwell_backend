33d37cf418426e28e69865d371d65a082af0103a
Mohanapriyan1386K <mohanapriyan.k.mfedu@gmail.com>
2026-06-22 23:34:27 +0530

    serveradd fixed

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import postsRouter from "./routes/posts.js";
 import jobsRouter from "./routes/jobs.js";
 import companyRoutes from "./routes/companyRoutes.js"
 import categoriesRouter from "./routes/categories.js";
+import authRoutes from "./routes/auth.js";
+import JobRoleRoutes from "./routes/Jobrole.js";
 import connectDB from "./db.js";
 
 const app = express();
@@ -29,6 +31,8 @@ app.use("/api/stories", postsRouter);
 app.use("/api/jobs", jobsRouter);
 app.use("/api/categories", categoriesRouter);
 app.use("/api/company", companyRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/jobsRole", JobRoleRoutes)
 app.use((req, res) => {
   res.status(404).json({ error: "Not found" });
 });
